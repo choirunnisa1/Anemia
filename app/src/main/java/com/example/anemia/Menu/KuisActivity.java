474e6ba52e538710dd93fa81f8a6ca8a2c7849d1
Choirunnisa1 <nisaninda3@gmail.com>
2019-08-12 20:09:30 +0700

    coba lcm

--- app/src/main/java/com/example/anemia/Menu/KuisActivity.java
+++ app/src/main/java/com/example/anemia/Menu/KuisActivity.java
@@ -1,16 +1,48 @@
 package com.example.anemia.Menu;
 
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.anemia.R;
 
+import java.util.Random;
+
 public class KuisActivity extends AppCompatActivity {
+
+    public int n = 15;
+    public static int[] Xn;
+    public int Xn0 ;
+    public int i;
+    public int a;
+    public int c;
+    public int m = 10;
+    public int Acak;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kuis);
+
+        Xn = new int[n];
+        Random b = new Random();
+            Xn0 = b.nextInt(m - 0 )+0;
+
+            for (i=1;i<=11;i++){
+                if (i==1){
+                    Xn[i] = ( a * Xn0 + c ) % m;
+                }
+                else if (i > 1 && i <= 10){
+                    Xn[i] = (a * Xn[i-1] + c) %m;
+                }
+                else {
+
+                    break;
+                }
+
+            }
 
 
     }
