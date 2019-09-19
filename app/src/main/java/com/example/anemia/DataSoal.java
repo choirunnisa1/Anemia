048bbfb4635af2c371c413785e70e4a990c1af43
Iav <ade.fajr.ariav@gmail.com>
2019-09-19 18:48:52 +0700

    fixed 1

--- app/src/main/java/com/example/anemia/DataSoal.java
+++ app/src/main/java/com/example/anemia/DataSoal.java
@@ -11,7 +11,7 @@ import static java.util.concurrent.ThreadLocalRandom.current;
 
 public class DataSoal {
     private String id[]={
-            "1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"
+            "1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"
 
     };
     private String pertanyaan[]={
--- app/src/main/java/com/example/anemia/Menu/KuisActivity.java
+++ app/src/main/java/com/example/anemia/Menu/KuisActivity.java
@@ -1,6 +1,7 @@
 package com.example.anemia.Menu;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,7 +22,7 @@ import java.util.Random;
 
 public class KuisActivity extends AppCompatActivity {
 
-//    public int n = 15;
+    //    public int n = 15;
 //    public static int[] Xn;
 //    public int Xn0;
 //    public int i;
@@ -29,52 +30,24 @@ public class KuisActivity extends AppCompatActivity {
 //    public int c;
 //    public int m = 10;
 //    public int Acak;
-   DataSoal dataSoal;
+    DataSoal dataSoal;
     double skor, benar;
     RadioGroup grupoption;
-    RadioButton optiona, optionb, optionc,optiond;
-    TextView no_soal,tv_soal;
+    RadioButton optiona, optionb, optionc, optiond;
+    TextView no_soal, tv_soal;
     Button buttonnext;
     String jawaban;
     public static int[] Xn;
     public int Xn0;
-    public int i,a,c,m;
+    public int i, a, c, m;
+    public int soalQuis;
     public int nilaiAcak;
-
-    public void lcm() {
-      int n;
-        n = 20;
-        a = 13;
-        m = 23;
-        c = 5;
-        Xn = new int[n];
-
-        Random b = new Random();
-        Xn0 = b.nextInt(m - 0) + 0;
-
-        for (i = 1; i <= 11; i++) {
-
-            if (i == 1) {
-                Xn[i] = (a * Xn0 + c) % m;
-            } else if (i > 1 && i <= 10) {
-                Xn[i] = (a * Xn[i - 1] + c) % m;
-            } else {
-                Log.d("Algoritma LCM", "LCM Selesai");
-                break;
-            }
-
-            nilaiAcak = Xn[i];
-
-            Log.d("Hasil Algoritma LCM","Xn[" + i + "] = " + nilaiAcak);
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kuis);
 
-        lcm();
 
         grupoption = findViewById(R.id.rgop);
         tv_soal = findViewById(R.id.soal);
@@ -84,7 +57,9 @@ public class KuisActivity extends AppCompatActivity {
         optionc = findViewById(R.id.optionc);
         optiond = findViewById(R.id.optiond);
         buttonnext = findViewById(R.id.submit);
+        dataSoal = new DataSoal();
 
+        lcm();
         update();
         buttonnext.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,19 +68,47 @@ public class KuisActivity extends AppCompatActivity {
             }
         });
     }
+    public void lcm() {
+        int n;
+        n = 20;
+        a = 13;
+        m = 23;
+        c = 5;
+        Xn = new int[n];
+
+        Random b = new Random();
+        Xn0 = b.nextInt(m - 0) + 0;
+
+        for (i = 1; i == 1; i++) {
+
+            if (i == 1) {
+                Xn[i] = (a * Xn0 + c) % m;
+            } else if (i > 1 && i == 1) {
+                Xn[i] = (a * Xn[i - 1] + c) % m;
+            } else {
+                Log.d("Algoritma LCM", "LCM Selesai");
+                break;
+            }
+
+            nilaiAcak = Xn[i];
+
+            Log.d("Hasil Algoritma LCM", "Xn[" + i + "] = " + nilaiAcak);
+        }
+    }
 
 
     public void update() {
         grupoption.clearCheck();
-        lcm();
-        if (i == 10) {
+        Log.d("Nilai Acak", ">> " + nilaiAcak);
+        nilaiAcak = nilaiAcak - 1;
+        if (soalQuis == 10) {
             skor = (benar * 10);
             String skorx = String.valueOf(skor);
             Intent i = new Intent(KuisActivity.this, SkorActivity.class);
             i.putExtra("skorakhir", skorx);
             startActivity(i);
         } else {
-            no_soal.setText(dataSoal.getId(nilaiAcak));
+            no_soal.setText(dataSoal.getId(nilaiAcak) + ">> " + i);
             tv_soal.setText(dataSoal.getSoal(nilaiAcak));
             optiona.setText(dataSoal.getOpsi1(nilaiAcak));
             optionb.setText(dataSoal.getOpsi2(nilaiAcak));
@@ -133,54 +136,67 @@ public class KuisActivity extends AppCompatActivity {
 //        z++;
 //    }
 
-    public void cekJawaban(){
-        if (optiona.isChecked()){
-            if(optiona.getText().toString().equals(jawaban)){
+    public void cekJawaban() {
+        lcm();
+        if (optiona.isChecked()) {
+            if (optiona.getText().toString().equals(jawaban)) {
                 benarloh();
-            }else{
+                endQuis();
+            } else {
                 Toast.makeText(getApplicationContext(), "Anda salah", Toast.LENGTH_SHORT).show();
                 salah();
                 update();
+                endQuis();
             }
         }
-        if (optionb.isChecked()){
-            if(optionb.getText().toString().equals(jawaban)){
+        if (optionb.isChecked()) {
+            if (optionb.getText().toString().equals(jawaban)) {
                 benarloh();
-            }else{
+                endQuis();
+            } else {
                 Toast.makeText(getApplicationContext(), "Anda salah", Toast.LENGTH_SHORT).show();
                 salah();
                 update();
+                endQuis();
             }
         }
-        if (optionc.isChecked()){
-            if(optionc.getText().toString().equals(jawaban)){
+        if (optionc.isChecked()) {
+            if (optionc.getText().toString().equals(jawaban)) {
                 benarloh();
-            }else{
+                endQuis();
+            } else {
                 Toast.makeText(getApplicationContext(), "Anda salah", Toast.LENGTH_SHORT).show();
                 salah();
                 update();
+                endQuis();
             }
         }
-        if (optiond.isChecked()){
-            if(optiond.getText().toString().equals(jawaban)){
+        if (optiond.isChecked()) {
+            if (optiond.getText().toString().equals(jawaban)) {
                 benarloh();
-            }else{
+                endQuis();
+            } else {
                 Toast.makeText(getApplicationContext(), "Anda salah", Toast.LENGTH_SHORT).show();
                 salah();
                 update();
-            }}
+                endQuis();
+            }
+        }
     }
 
-    public void benarloh(){
-        benar=benar+1;
+    public void benarloh() {
+        benar = benar + 1;
         Toast.makeText(getApplicationContext(), "Anda benar", Toast.LENGTH_SHORT).show();
         update();
     }
 
-    public void salah(){
-        benar=benar+0;
-        }
+    public void endQuis(){
+        soalQuis = soalQuis + 1;
+    }
 
+    public void salah() {
+        benar = benar + 0;
+    }
 
 
 //    public void AcakKuis(){
@@ -205,9 +221,9 @@ public class KuisActivity extends AppCompatActivity {
 //
 
 
-//}
-@Override
-public void onBackPressed(){
-    Toast.makeText(getApplicationContext(), "Tidak bisa kembali ke kuis. Silahkan selesaikan terlebih dahulu", Toast.LENGTH_SHORT).show();
-}
+    //}
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(getApplicationContext(), "Tidak bisa kembali ke kuis. Silahkan selesaikan terlebih dahulu", Toast.LENGTH_SHORT).show();
+    }
 }
